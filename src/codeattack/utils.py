78020f92b58fcc4681efe95050cdcadc12c7c78f
Marco Rosa <marco.rosa@sap.com>
2024-11-19 17:22:18 +0100

    Support mistral

--- src/codeattack/utils.py
+++ src/codeattack/utils.py
@@ -4,10 +4,11 @@ from openai import OpenAI
 from typing import Union, List
 
 
-def api_call(client, query: Union[List, str],
-             model_name="gpt-4o",
+def api_call(client,
+             query: Union[List, str],
+             model_name: str,
              response_format='text',
-             temperature=1):
+             temperature=1.0):
     if isinstance(query, List):
         messages = query
     elif isinstance(query, str):
@@ -37,17 +38,18 @@ def get_client(model_name):
         BASE_URL_GPT = os.getenv("BASE_URL_GPT")
         gpt_client = OpenAI(base_url=BASE_URL_GPT, api_key=API_KEY)
         return gpt_client
+    elif 'mistral' in model_name:
+        # mistral's API are OpenAI-compatible
+        API_KEY = os.getenv('MISTRAL_API_KEY')
+        BASE_URL_MISTRAL = os.getenv("BASE_URL_MISTRAL",
+                                     "https://api.mistral.ai/v1")
+        mistral_client = OpenAI(base_url=BASE_URL_MISTRAL, api_key=API_KEY)
+        return mistral_client
     elif 'claude' in model_name:
+        # TODO: implement this client
         claude_client = ''
         return claude_client
     else:
+        # TODO: implement this client
         llama_client = ''
         return llama_client
-
-
-if __name__ == '__main__':
-    query = "I have an array a with length of n, the function f(l, r) represents the sum of numbers in a from index l to r. I can re-sort the array a, to minimize the sum of all intervals"
-    # model_name = "claude-3-5-sonnet-20240620"
-    model_name = "gpt-4o"
-    client = get_client(model_name)
-    print(api_call(client, query, model_name))
